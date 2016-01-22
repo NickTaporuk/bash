56b32e3682db66191560ea5ab6e2551f676d1a23
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2016-01-22 10:05:33 +0200

    add symfony clear cache bash script v1.1

--- sf_clear_cache.sh
+++ sf_clear_cache.sh
@@ -1,1 +1,17 @@
-read
+#!/usr/bin/env bash
+PWD=`pwd`;
+CMD='app/console cache:clear';
+COMMAND=''
+sudo chmod -R 0777 $PWD ;
+
+if [[ -n "$1" && "$1" -ne 'y' ]];
+ then COMMAND="$CMD --env=$1";
+ else COMMAND="$CMD --env=dev";
+fi
+
+if [[ "$2" = "y" || "$1" = "y" ]];
+ then COMMAND="$COMMAND --no-warmup"
+fi
+
+$COMMAND;
+sudo chmod -R 0777 $PWD;
